3ae778cd68bb56f0279ac539a1d0439361c39ebc
GrO0vy <lmc2819@naver.com>
2023-06-15 23:17:51 +0900

    week11-day4 tasks

--- jpa/src/main/java/com/example/jpa/AppService.java
+++ jpa/src/main/java/com/example/jpa/AppService.java
@@ -20,7 +20,7 @@ public class AppService {
     }
 
     // CREATE
-    public void createStudent(String name, Integer age, String phone, String email){
+    public StudentEntity createStudent(String name, Integer age, String phone, String email){
        // 새로운 Student를 만들고 싶다.
         StudentEntity newEntity = new StudentEntity();
         newEntity.setName(name);
@@ -29,7 +29,9 @@ public class AppService {
         newEntity.setEmail(email);
 
         // 저장
-        this.studentRepository.save(newEntity);
+        // save() 메소드는 생성된 엔티티를 반환한다.
+        newEntity = this.studentRepository.save(newEntity);
+        return newEntity;
     }
 
     // READ ALL
@@ -39,11 +41,12 @@ public class AppService {
 
         List<StudentDto> studentDtoList = new ArrayList<>();
         for(StudentEntity studentEntity : this.studentRepository.findAll()){
-            StudentDto studentDto = new StudentDto();
-            studentDto.setId(studentEntity.getId());
-            studentDto.setName(studentEntity.getName());
-            studentDto.setEmail(studentEntity.getEmail());
-            studentDtoList.add(studentDto);
+//            StudentDto studentDto = new StudentDto();
+//            studentDto.setId(studentEntity.getId());
+//            studentDto.setName(studentEntity.getName());
+//            studentDto.setEmail(studentEntity.getEmail());
+//            studentDtoList.add(studentDto);
+            studentDtoList.add(StudentDto.fromEntity(studentEntity));
         }
         //return studentEntityList;
         return studentDtoList;
--- jpa/src/main/java/com/example/jpa/dto/StudentDto.java
+++ jpa/src/main/java/com/example/jpa/dto/StudentDto.java
@@ -1,5 +1,6 @@
 package com.example.jpa.dto;
 
+import com.example.jpa.entities.StudentEntity;
 import lombok.Data;
 
 @Data
@@ -7,4 +8,13 @@ public class StudentDto {
     private Long id; //Entity.id
     private String name; // Entitiy.name
     private String email; // Entity.email
+
+    //정적 팩토리 메소드 패턴
+    public static StudentDto fromEntity(StudentEntity studentEntity){
+        StudentDto dto = new StudentDto();
+        dto.setId(studentEntity.getId());
+        dto.setName(studentEntity.getName());
+        dto.setName(studentEntity.getEmail());
+        return dto;
+    }
 }
